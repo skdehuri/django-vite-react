90db93b7eca884dcd8999ce3567d38e750b24eb3
Swaraj <swaraj.dehuri@gmail.com>
2023-07-02 14:21:38 +0530

    django vite initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // root: resolve('./frontend/')
+  build: {
+    manifest: true,
+    outDir: 'frontend/dist',
+  }
+})
