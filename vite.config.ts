0c0937fd098b3faa980a50f257aa87d901621752
Swaraj <swaraj.dehuri@gmail.com>
2023-07-03 21:11:35 +0530

    changes for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,23 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/static/',
+  server: {
+    origin: 'http://127.0.0.1:3000',
+    host: 'localhost',
+    port: 3000,
+    open: false,
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+  },
   build: {
     manifest: true,
     outDir: 'static/frontend/dist/',
     rollupOptions: {
       input: {
         main: 'static/frontend/src/main.tsx',
-        todo: 'static/frontend/src/components/todo/main.tsx'
+        todo: 'static/frontend/src/components/todo/main.tsx',
       }
     },
   }
