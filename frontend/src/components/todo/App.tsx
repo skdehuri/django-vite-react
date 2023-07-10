351eb6bbc1e8ce9f38d68a52fdc46403c6b3d4b6
Swaraj <swaraj.dehuri@gmail.com>
2023-07-10 14:47:15 +0530

    env variable and editor config changes

--- frontend/src/components/todo/App.tsx
+++ frontend/src/components/todo/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-        <h1>TODO APP</h1>
-        <h2>React + Vite</h2>
-        <p>test</p>
+      <h1>TODO APP</h1>
+      <h2>React + Vite</h2>
+      <p>test</p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/static/',
-  server: {
-    origin: 'http://127.0.0.1:3000',
-    host: 'localhost',
-    port: 3000,
-    open: false,
-    watch: {
-      usePolling: true,
-      disableGlobbing: false,
+export default defineConfig(({ mode }) => {
+  // const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: '/static/',
+    server: {
+      origin: 'http://127.0.0.1:3000',
+      host: 'localhost',
+      port: 3000,
+      open: false,
+      watch: {
+        usePolling: true,
+        disableGlobbing: false,
+      },
     },
-  },
-  build: {
-    manifest: true,
-    outDir: 'static/dist/',
-    rollupOptions: {
-      input: {
-        main: 'frontend/src/main.tsx',
-        todo: 'frontend/src/components/todo/main.tsx',
-      }
-    },
+    build: {
+      manifest: true,
+      sourcemap: true,
+      outDir: 'static/dist/',
+      rollupOptions: {
+        input: {
+          main: 'frontend/src/main.tsx',
+          todo: 'frontend/src/components/todo/main.tsx',
+        }
+      },
+    }
   }
 })
