5c1a6fcec4bacf52537efc0dc161d8a38136c21a
Swaraj <swaraj.dehuri@gmail.com>
2023-07-15 20:49:08 +0530

    postgres settings added

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+        },
     }
 }
 
